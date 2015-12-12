0b21915d445a8d88c54caaf0c29047c3614153a0
X-Engineer-001 <fabin1790@gmail.com>
2015-12-12 12:54:29 +0800

    TD002

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var bgimg=document.createElement("img");
 bgimg.src="images/map.png";
 var chtimg=document.createElement("img");
 chtimg.src="images/slime.gif";
-var canvas=document.getElementsById("gamecanvas");
+var canvas=document.getElementById("gamecanvas");
 var ctx=canvas.getContext("2d");
 function draw(){
   ctx.drawImage(bgimg,0,0);

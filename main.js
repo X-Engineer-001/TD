ab503aa4cb4de70cecd3a12a60c92d8190fd382b
X-Engineer-001 <fabin1790@gmail.com>
2016-01-22 19:30:03 +0800

    TD

--- main.js
+++ main.js
@@ -135,12 +135,12 @@ function Enemy(){
     if(this.direction.x!=0&&iscollided((this.x-((this.x%32)*this.direction.x))+(32*this.direction.x),this.y,
       this.x,this.y,this.speed/FPS,this.speed/FPS
     )){
-      this.x=(this.x-(this.x%32))+32
+      this.x=(this.x-((this.x%32)*this.direction.x))+(32*this.direction.x);
     }
     if(this.direction.y!=0&&iscollided(this.x,(this.y-((this.y%32)*this.direction.y))+(32*this.direction.y),
       this.x,this.y,this.speed/FPS,this.speed/FPS
     )){
-      this.y=(this.y-(this.y%32))+32
+      this.y=(this.y-((this.y%32)*this.direction.y))+(32*this.direction.y);
     }
     if(this.x%32==0&&this.y%32==0&&this.delay<=0){
       this.delay=FPS/4;
